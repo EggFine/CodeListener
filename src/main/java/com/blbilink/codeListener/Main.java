f08d4b3538d8635a71086438112e4849fbbd639c
EggFine <1079742633@qq.com>
2024-08-04 19:47:18 +0800

    update

--- src/main/java/com/blbilink/codeListener/Main.java
+++ src/main/java/com/blbilink/codeListener/Main.java
@@ -1,20 +1,57 @@
 package com.blbilink.codeListener;
 
 import java.io.IOException;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] str) throws IOException {
-        Scanner scanner;
-        List<String> path = new ArrayList<>();
-        int num = 1;
+    static String listenPath;
+    static List<String> corePath = new ArrayList<>();
+    static int coreNum;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] str) {
+        System.out.println("请输入监听路径: ");
+        listenPath = scanner.next();
+
+        for (coreNum = 1; ; coreNum++) {
+            String cache;
+            System.out.println("（输入 stop 结束）请输入第" + coreNum + "个核心路径: ");
+            cache = scanner.next();
+            if (cache != "stop") {
+                corePath.add(cache);
+            } else {
+                break;
+
+            }
+        }
+
+    }
 
 
-        System.out.println("请输入第 " + num + "个路径");
-        scanner = new Scanner(System.in);
-        path.add(scanner.next());
-        System.out.println(path);
+    private void dirListener(String dir) throws IOException {
+        WatchService watcher = FileSystems.getDefault().newWatchService();
+        Path path = Paths.get(dir);
+        path.register(watcher, StandardWatchEventKinds.ENTRY_MODIFY);
+
+        try {
+            while (true) {
+                WatchKey key = watcher.take();
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    if (event.kind() == StandardWatchEventKinds.OVERFLOW) {
+                        continue;
+                    }
+                    Path fileName = (Path) event.context();
+                    System.out.println("文件更新: " + fileName);
+                }
+                if (!key.reset()) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
